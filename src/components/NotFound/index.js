fd7d205893dce609ce43bda5d6a5157765be3f19
Diego Delgado <diego.delgadop1992@gmail.com>
2020-02-08 09:31:21 -0400

    Updated not found page style

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import NotFoundImage from 'assets/images/notfound.png';
+import NotFoundImage from 'assets/images/notfound.PNG';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Link } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	image: {
+		marginTop: 40,
+		width: '80%',
+		borderRadius: 10
+	},
+	button: {
+		marginTop: 50,
+		width: 210,
+		backgroundColor: '#4B4B49'
+	}
+}));
 
 const NotFound = props => {
+	const classes = useStyles();
+
 	return (
-		<div>
-			<img src={NotFoundImage} alt="not found" title="not found page"></img>
+		<div className={classes.container}>
+			<img src={NotFoundImage} alt="not found" title="not found page" className={classes.image} />
+			<Button variant="contained" className={classes.button}>
+				<Link variant="button" href="/" style={{ color: 'white' }}>
+					Home
+				</Link>
+			</Button>
 		</div>
 	);
 };

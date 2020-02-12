a923f77bab50f9926fcedc574311281b28f5a4e6
Diego Delgado <diego.delgado@endava.com>
2020-02-12 09:33:41 -0400

    Removed lodash

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,7 +1,6 @@
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-import debounce from 'lodash/debounce';
 import React from 'react';
 import { searchMovie, fetchMovies } from 'redux/actions/moviesActions';
 import { connect } from 'react-redux';
